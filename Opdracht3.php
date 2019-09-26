84c33cb594b0a430c5f906651aa4ef2248f004c0
FireWolf16 <Patrick.laranjo@gmail.com>
2019-09-26 11:22:58 +0200

    opdracht3

--- Opdracht3.php
+++ Opdracht3.php
@@ -1,7 +1,17 @@
-<?php
-$boodschappen = ["aardappelen", "aardbeien", "3 pakken melk", "yoghurt"];
+<html>
 
-$boodschappen = array("aardappelen", "aardbeien", "3 pakken melk", "yoghurt");
-foreach ($boodschappen as $boodschap) {
-    echo "$boodschap <br> <img alt='$boodschap' src='img/$boodschap.jpg'> <br>";
-}
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <?php
+
+    $boodschappen = array("aardappelen", "aardbeien", "3 pakken melk", "yoghurt");
+    foreach ($boodschappen as $boodschap) {
+        echo "$boodschap <br> <img alt='$boodschap' src='img/$boodschap.jpg'> <br>";
+    }
+    ?>
+</body>
+
+</html>
